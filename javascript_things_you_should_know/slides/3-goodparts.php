38c70dc1647ca207a5d4967c4ac748fb0d35305e
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-10-02 18:40:28 +0200

    master better slides

--- javascript_things_you_should_know/slides/3-goodparts.php
+++ javascript_things_you_should_know/slides/3-goodparts.php
@@ -3,11 +3,21 @@
     <h2>Good parts</h2>
     <ul>
       <li style="padding: 2px 0;" class="fragment fade-in">Loose typing</li>
-      <li style="padding: 2px 0;" class="fragment fade-in">Functions as first class objects</li>
+      <li style="padding: 2px 0;" class="fragment fade-in">Functions</li>
+      <li style="padding: 2px 0;" class="fragment fade-in">Expressive object literal</li>
       <li style="padding: 2px 0;" class="fragment fade-in">Dynamic objects</li>
-      <li style="padding: 2px 0;" class="fragment fade-in">Expresive object literal</li>
       <li style="padding: 2px 0;" class="fragment fade-in">Function scope / closure</li>
     </ul>
+
+    <aside class="notes">
+      <ul>
+        <li>Loose typing in de browser is een goed ding, vergelijkbaar dus met php.</li>
+        <li>Functies als eerste klas objecten.</li>
+        <li>De mannier waarop javascript objecten worden gevormd, vergelijkbaar en deels overgenomen uit JSON.</li>
+        <li>Dynamische objecten, aanpassen van objecten is een makkie.</li>
+        <li>In Javascript hebben we functio scope en closures.</li>
+      </ul>
+    </aside>
   </section>
 
   <section>
@@ -15,33 +25,45 @@
     <ul>
       <li>Primitives: <a>Number</a>, <a>Boolean</a>, <a>String</a>.</li>
       <li>Objects: <a>Object</a>, <a>Function</a>, <a>Array</a>, <a>Date</a>, <a>RegExp</a>, etc.</li>
-      <li>Special cases: <a>Null</a>, <a>Undefined</a>.</li>
+      <li>Special cases: <a>Null</a>, <a>Undefined</a>, <a>NaN</a>.</li>
     </ul>
-  </section>
-
-  <section>
-    <h2>Loose typing</h2>
-    <br>
+    <br><br>
     <pre><code data-trim contenteditable>
 var a = 13; // Number declaration
-var b = "thirteen"; // String declaration
+var b = "beee"; // String declaration
+var c = true; // Boolean declaration
+var d = []; // Array declaration
+var e = {}; // Object declaration
+var f = function(){}; // Function declaration
     </code></pre>
     <br>
-    <ul>
-      <li>Loose typing means that variables are declared without a type.</li>
-      <li>Variables in JavaScript are typed, but that type is determined internally.</li>
-    </ul>
+
+    <aside class="notes">
+      <ul>
+        <li>Onder te verdelen in 3 grote groepen</li>
+        <li>Loosely typed wil gewoon zeggen dat elke variabele in javascript automatisch een type krijgt, je hoeft niet te zeggen wat je variabele zal zijn.</li>
+        <li>De compiler detecteerd welk type je declareerd.</li>
+      <ul>
+    </aside>
   </section>
 
   <section>
     <h2>Functions as first class objects</h2>
     <ul>
-      <li>A function is an instance of the Object type</li>
-      <li>A function can have properties and has a link back to its constructor method</li>
-      <li>You can store the function in a variable</li>
-      <li>You can pass the function as a parameter to another function</li>
-      <li>You can return the function from a function</li>
+      <li>Instance of the Object type</li>
+      <li>Store in a variable</li>
+      <li>As a parameter to another function</li>
+      <li>Return a function from another function</li>
     </ul>
+
+    <aside class="notes">
+      <ul>
+        <li>Een functie in javascript is een instantie van een object.</li>
+        <li>Je kan functies opslaan in variabelen</li>
+        <li>Je kan functies doorgeven aan functies</li>
+        <li>Je kan functies terug geven vanuit functies</li>
+      </ul>
+    </aside>
   </section>
 
   <section>
@@ -58,24 +80,18 @@ Object.getOwnPropertyNames(obj).forEach(function(val, idx, array) {
   console.log(val + " -> " + obj[val]);
 });
 };
+// consoles
+// "prototype -> [object Object]"
+// "length -> 1"
+// "name -> test"
 
 getKeys(myBarFunc);
+
     </code></pre>
-
-    <p>consoles:</p>
-
-    <blockquote style="text-align: left;">
-      "prototype -> [object Object]"<br>
-      "length -> 1"<br>
-      "name -> test"<br>
-    </blockquote>
   </section>
 
   <section>
-    <h2>Expresive object literal</h2>
-    <ul>
-      <li>JSON like object literal</li>
-    </ul>
+    <h2>Expressive object literal</h2>
     <pre><code data-trim contenteditable>
 var person = {
   firstName: "John",
@@ -90,10 +106,6 @@ var person = {
 
   <section>
     <h2>Dynamic objects</h2>
-    <ul>
-      <li>You can fully modify objects at runtime</li>
-    </ul>
-
     <pre><code data-trim contenteditable>
 var person = {
   firstName: "John",
@@ -118,20 +130,13 @@ delete person.fireGun; //REMOVE
   </section>
   <section>
     <h2>Function scope</h2>
-
-    <ul>
-      <li style="padding: 2px 0;" class="fragment fade-in">Scope controls the visibility and lifetimes of variables and parameters.</li>
-      <li style="padding: 2px 0;" class="fragment fade-in">Reduces naming collisions and provides automatic memory management.</li>
-      <li style="padding: 2px 0;" class="fragment fade-in">Can someone see the error?</li>
-    </ul>
-
-    <pre class="fragment fade-in"><code data-trim contenteditable>
+    <pre><code data-trim contenteditable>
 var foo = function() {
   var a = 3, b = 5;
 
   var bar = function() {
     var b = 7, c = 11;
-    // At this point, a is 3, b is 7 and c is 11
+    /// At this point, a is 3, b is 7 and c is 11
 
     a += b + c;
     // At this point, a is 21, b is 7 and c is 11
@@ -140,17 +145,22 @@ var foo = function() {
 
   bar();
   // At this point, a is 21, b is 5
+
 }
     </code></pre>
+
+    <aside class="notes">
+      <ul>
+        <li>Scope zorgt voor de zichtbaarheid en duurbaarheid van variabelen en parameters</li>
+        <li>Scope reduceerd ook naam overlapping en scope zorgt ook voor memory management</li>
+        <li>Ziet iemand hier een error</li>
+      </ul>
+    </aside>
   </section>
   <section>
     <h2>Closure</h2>
-    <ul>
-      <li style="padding: 2px 0;" class="fragment fade-in">Whenever you see the function keyword within another function, the inner function has access to variables in the outer function context.</li>
-      <li style="padding: 2px 0;" class="fragment fade-in">You can understand this best by example.</li>
-    </ul>
 
-    <pre class="fragment fade-in"><code data-trim contenteditable>
+    <pre><code data-trim contenteditable>
 function foo(x) {
   var tmp = 3;
   return function (y) {
@@ -173,6 +183,13 @@ var quo = function(status) {
 var myQuo = quo("amazed");
 console.log(myQuo.get_status()); // outputs amazed
     </code></pre>
+
+    <aside class="notes">
+      <ul>
+        <li>Van zodra je het function keyword in een anderer function ziet, dan heeft de binnenste functie toegang tot de buitenste functie zijn context</li>
+        <li>closure werkt ook goed samen met this</li>
+      </ul>
+    </aside>
   </section>
 </section>
 
