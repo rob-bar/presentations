ea50facb2cc6a577746270ef5756abd3cc1cebc9
Robbie Bardijn <robbie.bardijn@crosscheck.be>
2015-05-21 09:32:27 +0200

    gh-pages: one html better

--- one_agency_fei_ocelot/slides/slide-5.php
+++ one_agency_fei_ocelot/slides/slide-5.php
@@ -1,10 +1,13 @@
 <section>
   <section>
+    <h1>Questions?</h1>
+  </section>
+
+  <section>
     <h2>Thanks <br> <small>and now...</small></h2>
   </section>
 
   <section>
     <img src="https://i.chzbgr.com/maxW500/8496825344/h6961D76D/" style="width: 900px" alt="one">
   </section>
-
 </section>
