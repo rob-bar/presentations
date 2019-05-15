0caa83766f862912adbe8b9c247519d4c253f894
Robbie Bardijn <robbie.bardijn@gmail.com>
2019-05-15 13:41:32 +0200

    master: react and colser

---
+++ colser_and_react/README.md
@@ -0,0 +1,3 @@
+### One Agency: FEI & Ocelot
+
+You can watch the presentation [here](http://rob-bar.github.io/presentations/one_agency_fei_ocelot/)
---
+++ colser_and_react/slides/slide-4.php
@@ -0,0 +1,75 @@
+<section data-background="img/pat.png">
+  <section>
+    <h2>Ocelot</h2>
+    <img src="https://i.chzbgr.com/maxW500/8496874496/hB4BDEF60/" style="width: 600px" alt="one">
+  </section>
+
+  <section>
+    <h3>What is it?</h3>
+    <ul>
+      <li class="fragment">Theming <a>toolbelt</a></li>
+      <li class="fragment">Mainly for <a>Drupal 7</a></li>
+      <li class="fragment">Decent & tested <a>standards</a></li>
+      <li class="fragment">Ready for project <a>kick-off</a></li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>Strengths and weaknesses</h3>
+    <ul style="list-style-type: none;">
+      <li class="fragment"><i class="fa fa-plus-circle"></i>&nbsp;&nbsp;Structure</a></li>
+      <li class="fragment"><i class="fa fa-plus-circle"></i>&nbsp;&nbsp;Themers on same config, always</li>
+      <li class="fragment"><i class="fa fa-plus-circle"></i>&nbsp;&nbsp;Good <a>linting</a> tools (scss & js)</li>
+      <li class="fragment"><i class="fa fa-plus-circle"></i>&nbsp;&nbsp;Many <a>helpers</a> & good <a>defaults</a></li>
+      <li class="fragment"><i class="fa fa-plus-circle"></i>&nbsp;&nbsp;<a>Smacss</a> based</li>
+    <br>
+      <li class="fragment"><i class="fa fa-minus-circle"></i>&nbsp;&nbsp;Compilation <a>Speed</a></a></li>
+      <li class="fragment"><i class="fa fa-minus-circle"></i>&nbsp;&nbsp;2 package managers (<a>gems</a> & <a>npm</a>)</li>
+      <li class="fragment"><i class="fa fa-minus-circle"></i>&nbsp;&nbsp;No plugable <a>components</a></li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>Now</h3>
+    <ul>
+      <li class="fragment">Current stable version <a href="https://github.com/Crosscheck/Ocelot/releases">0.1.2</a></li>
+      <li class="fragment">WIP <a href="https://github.com/Crosscheck/Ocelot/milestones/Version%200.1.3"> Milestone 0.1.3</a></li>
+      <li class="fragment">Integrated in <a href="https://github.com/Crosscheck/projectify">Projectify</a></li>
+      <li class="fragment">Wiki & Doc can be found <a href="https://github.com/Crosscheck/Ocelot/wiki">here</a></li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>Future plans and milestones</h3>
+  </section>
+
+  <section>
+    <h3>Milestone 0.1.3 <a>(35%)</a><br><small>Structure & general improvements</small></h3>
+    <ul style="list-style-type: none;">
+      <li class="fragment"><i class="fa fa-check-square-o"></i>&nbsp;&nbsp;Structure</li>
+      <li class="fragment"><i class="fa fa-check-square-o"></i>&nbsp;&nbsp;Better Doc & wiki</li>
+      <li class="fragment"><i class="fa fa-github"></i>&nbsp;&nbsp;Susy grid config and mixins</li>
+      <li class="fragment"><i class="fa fa-square-o"> Bower integration & injection into .info file</i></li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>Milestone 0.1.4 <br><small>Speed boosts and no more gems</small></h3>
+    <ul style="list-style-type: none;">
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Convert to libsass</li>
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Replace all the gems by npm modules</li>
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Replace compass with bourbon</li>
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Grunt or Gulp investigation</li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>Milestone 0.1.5 <br><small>Components, GPI and D8 standards</small></h3>
+    <ul style="list-style-type: none;">
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Make components plugable <br>(Messages, tabs, menu' s, etc.)</li>
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Integrate D8 standards (backbone, eshinting)</li>
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Google page speed insights on workflow</li>
+      <li class="fragment"><i class="fa fa-square-o"></i>&nbsp;&nbsp;Critical css</li>
+    </ul>
+  </section>
+</section>
